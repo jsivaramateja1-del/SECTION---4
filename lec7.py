c469148ce38f8d72c15135fc54c46dcef32d57c5
jsivaramateja1-del <jsivaramateja1@gmail.com>
2026-02-18 14:14:10 +0530

    lec7

---
+++ lec7.py
@@ -0,0 +1,19 @@
+'''
+print(type(5))
+print(type(5.0))
+print(type('5'))
+print(type(True))
+print(type(2+5j))
+'''
+'''
+S = "Sai"
+S1 = "Uni"
+S2 = S + S1
+#S3 = S - S1 #(Not defined for strings)
+print(S2)
+#print(S3)
+#S4 = S % S1 #(Not defined for strings, but we can use % operator for string formatting)
+#print(S4)
+# S5 = S * S1 #(Not defined for strings, but we can multiply a string with an integer)
+# print(S5)
+'''
